52783d3f0fa26841c6cc362f423e0b51ce5a3bed
Mustafa Uzun <mustafa.uzun@std.izmirekonomi.edu.tr>
2019-12-03 22:02:58 +0300

    Initial commit

---
+++ watchWall/frm_wallpaper.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace watchWall
+{
+    partial class frm_wallpaper
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbx_wallpaper = new System.Windows.Forms.PictureBox();
+            this.tmr_wallpaper = new System.Windows.Forms.Timer(this.components);
+            this.btn_run = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_wallpaper)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbx_wallpaper
+            // 
+            this.pbx_wallpaper.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pbx_wallpaper.Location = new System.Drawing.Point(0, 0);
+            this.pbx_wallpaper.Name = "pbx_wallpaper";
+            this.pbx_wallpaper.Size = new System.Drawing.Size(800, 402);
+            this.pbx_wallpaper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx_wallpaper.TabIndex = 0;
+            this.pbx_wallpaper.TabStop = false;
+            // 
+            // tmr_wallpaper
+            // 
+            this.tmr_wallpaper.Interval = 60000;
+            this.tmr_wallpaper.Tick += new System.EventHandler(this.tmr_wallpaper_Tick);
+            // 
+            // btn_run
+            // 
+            this.btn_run.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_run.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_run.Location = new System.Drawing.Point(12, 412);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(776, 27);
+            this.btn_run.TabIndex = 1;
+            this.btn_run.Text = "Çalıştır";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // frm_wallpaper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_run);
+            this.Controls.Add(this.pbx_wallpaper);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frm_wallpaper";
+            this.Text = "Watch Wall";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frm_wallpaper_FormClosed);
+            this.Load += new System.EventHandler(this.frm_wallpaper_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_wallpaper)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbx_wallpaper;
+        private System.Windows.Forms.Timer tmr_wallpaper;
+        private System.Windows.Forms.Button btn_run;
+    }
+}
+
---
+++ watchWall/frm_wallpaper.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace watchWall
+{
+    public partial class frm_wallpaper : Form
+    {
+        public frm_wallpaper()
+        {
+            InitializeComponent();
+        }
+
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+
+        string wallpaperPath = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop").GetValue("WallPaper").ToString();
+        string cacheWallpaperPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Microsoft\\Windows\\Themes\\CachedFiles");
+        private void frm_wallpaper_Load(object sender, EventArgs e)
+        {  
+            if (!File.Exists(wallpaperPath))
+            {
+                wallpaperPath = Directory.GetFiles(cacheWallpaperPath, "*.jpg").Single(item => item.Contains("1920"));
+            }
+
+            try
+            {
+                File.Copy(wallpaperPath, Path.Combine(cacheWallpaperPath, "..", "origin.jpg"), true);
+            }
+            catch { }
+
+            pbx_wallpaper.ImageLocation = wallpaperPath;
+        }
+
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            tmr_wallpaper.Start();
+            tmr_wallpaper_Tick(null, null);
+        }
+
+        string tempXML = null;
+        private void tmr_wallpaper_Tick(object sender, EventArgs e)
+        {
+            var xml = new XmlDocument();
+            using (var wc = new WebClient())
+            {
+                xml.LoadXml(wc.DownloadString("http://api.tavcan.com/xml/doviz.xml"));
+            }
+
+            if (tempXML != xml.OuterXml)
+            {
+                tempXML = xml.OuterXml;
+
+                XmlNodeList nodes = xml.SelectNodes("piyasa/*");
+
+                string output = null;
+                foreach (XmlNode node in nodes)
+                {
+                    if(node.Name == "tarih")
+                    {
+                        output += String.Format("{0}: {1}\n", node.Name, node.InnerText);
+                        continue;
+                    }
+
+                    output += String.Format("{0}: {1}\n", node.Name, node.ChildNodes[1].InnerText);
+                }
+
+                Bitmap bitmap = (Bitmap)Image.FromFile(wallpaperPath);
+
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Font arialFont = new Font("Arial", 15))
+                    {
+                        graphics.DrawString(output, arialFont, Brushes.White, new Point(Screen.PrimaryScreen.Bounds.Width + 100, 0));
+                    }
+                }
+
+                string savedPath = Path.Combine(cacheWallpaperPath, "desktop.jpg");
+
+                bitmap.Save(savedPath, ImageFormat.Jpeg);
+
+                SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, savedPath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+            }
+        }
+
+        private void frm_wallpaper_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, Path.Combine(cacheWallpaperPath, "..", "origin.jpg"), SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+
+        }
+    }
+}
